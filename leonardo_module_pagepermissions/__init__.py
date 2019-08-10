be73e8477b1eee1b26166a47160edbceae3d3810
Dominik Resl <nikicresl@gmail.com>
2019-08-10 10:59:09 +0200

    Update for new deploy

--- leonardo_module_pagepermissions/__init__.py
+++ leonardo_module_pagepermissions/__init__.py
@@ -1,20 +1,24 @@
 
 from django.apps import AppConfig
 
+try:
+    from local_settings import APPS
+except ImportError:
+    pass
 
 default_app_config = 'leonardo_module_pagepermissions.PagePermissonsConfig'
 
 
 class Default(object):
+    if 'leonardo_module_pagepermissions' in APPS:
+        apps = [
+            'pagepermissions',
+            'leonardo_module_pagepermissions',
+        ]
 
-    apps = [
-        'pagepermissions',
-        'leonardo_module_pagepermissions',
-    ]
-
-    page_extensions = [
-        'pagepermissions.extension',
-    ]
+        page_extensions = [
+            'pagepermissions.extension',
+        ]
 
 
 class PagePermissonsConfig(AppConfig, Default):
